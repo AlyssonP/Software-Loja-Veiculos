cb80b4bf8bd2d686c78ccdba3c38d6d9b86d6e5c
AlyssonP <alysson.nascimento2111@gmail.com>
2023-06-16 11:52:23 -0300

    Create Project

---
+++ LojaVeiculos/src/main/java/Classes/Main.java
@@ -0,0 +1,10 @@
+package Classes;
+/**
+ *
+ * @author Alysson Pereira
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Loja de Veículos.");
+    }
+}
---
+++ LojaVeiculos/src/main/java/Classes/Veiculo.java
@@ -0,0 +1,8 @@
+package Classes;
+/**
+ *
+ * @author Alysson Pereira
+ */
+public class Veiculo {
+    
+}
